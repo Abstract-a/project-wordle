b2f08dea215a6b64a94762e418456a4dd05e65ed
Abstract-a <m.kamari100@gmail.com>
2023-12-23 15:23:44 +0100

    exo 2 : keeping track of guesses my sol

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import '../../styles.css';
+import GuessList from '../GuessList';
 
 const GuessInput = () => {
   const [guess, setGuess] = useState('');
+  const [guessId, setGuessId] = useState(0);
+  const [guessList, setGuessList] = useState([]);
 
   function handleChange(e) {
     setGuess(e.target.value.toUpperCase());
@@ -11,14 +14,18 @@ const GuessInput = () => {
   function handleSubmit(e) {
     e.preventDefault();
     console.log({ guess });
+    setGuessList([...guessList, { guess: guess, id: guessId }]);
+    setGuessId(guessId + 1);
     setGuess('');
   }
 
   return (
     <>
+      <GuessList guesses={guessList} />
       <form className="guess-input-wrapper" onSubmit={handleSubmit}>
         <label htmlFor="guess">Enter a Guess</label>
         <input
+          required
           pattern="\w{5,5}"
           title="the word has 5 letters"
           type="text"
