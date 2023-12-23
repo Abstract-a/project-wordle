97abd3aa0c04ee2def3035d1d274ce93cec69422
Abstract-a <m.kamari100@gmail.com>
2023-12-23 14:45:49 +0100

    quick fix

--- src/components/GuessInput.js
+++ src/components/GuessInput.js
@@ -5,7 +5,7 @@ const GuessInput = () => {
   const [guess, setGuess] = useState({ guess: '' });
 
   function handleChange(e) {
-    setGuess({ ...guess, guess: e.target.value });
+    setGuess({ ...guess, guess: e.target.value.toUpperCase() });
   }
 
   function handleSubmit(e) {
@@ -19,6 +19,8 @@ const GuessInput = () => {
       <form className="guess-input-wrapper" onSubmit={handleSubmit}>
         <label htmlFor="guess">Enter a Guess</label>
         <input
+          pattern="\w{5,5}"
+          title="the word has 5 letters"
           type="text"
           name="guess"
           id="guess-input"
